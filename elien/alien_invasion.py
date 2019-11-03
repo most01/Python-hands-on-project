ee08f8f8d37158ddc604315ae9216551eec8904c
user3 <user3@163.com>
2019-11-04 00:52:51 +0800

    12.3.2 设置背景色

--- elien/alien_invasion.py
+++ elien/alien_invasion.py
@@ -8,6 +8,9 @@ def run_game():
     screen = pygame.display.set_mode((1200,800))
     pygame.display.set_caption("Alien Invasion")
 
+   # 设置背景色
+    bg_color = (230, 230, 230)
+
     # 开始游戏的主循环
     while True:
 
@@ -15,6 +18,9 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        # 每次循环时都重绘屏幕
+        screen.fill(bg_color)
 
         # 让最近绘制的屏幕可见
         pygame.display.flip()
