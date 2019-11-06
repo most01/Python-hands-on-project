2c33cc3931985dbe19dce612fec4b1c82250676b
user3 <user3@163.com>
2019-11-06 22:04:21 +0800

    12.7.4

--- elien/game_functions.py
+++ elien/game_functions.py
@@ -1,6 +1,24 @@
 import sys
 import pygame
 
+def check_keydown_events(event, ship):
+    """响应按键"""
+    if event.key == pygame.K_RIGHT:
+        # 向右移动飞船
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        # 向右移动飞船
+        ship.moving_left = True
+
+def check_keyup_events(event, ship):
+    """响应松开"""
+    if event.key == pygame.K_RIGHT:
+        # 向右移动飞船
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        # 向右移动飞船
+        ship.moving_left = False
+
 def check_events(ship):
     """监视键盘和鼠标事件"""
     for event in pygame.event.get():
@@ -8,20 +26,10 @@ def check_events(ship):
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                # 向右移动飞船
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                # 向右移动飞船
-                ship.moving_left = True
+            check_keydown_events(event, ship)
 
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                # 向右移动飞船
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                # 向右移动飞船
-                ship.moving_left = False
+            check_keyup_events(event, ship)
 
 def update_screen(ai_settings, screen, ship):
     """更新屏幕上的图像，并切换到新屏幕"""
--- elien/ship.py
+++ elien/ship.py
@@ -2,9 +2,10 @@ import pygame
 
 class Ship():
 
-    def __init__(self,screen):
+    def __init__(self, ai_settings, screen):
         """初始化飞船并设置其初始位置"""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # 加载飞船图像并获取其外接矩形
         self.image = pygame.image.load('images/ship.bmp')
@@ -14,6 +15,9 @@ class Ship():
         # 将每艘新飞船放在屏幕底部中央
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+
+        # 在飞船的属性center中存储小数值
+        self.center = float(self.rect.centerx)
 
         # 移动标志
         self.moving_right = False
